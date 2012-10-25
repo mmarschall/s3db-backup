00e8423f4d9ef1bbb19f36585a0c85754bdca4ee
Matthias Marschall <mm@agileweboperations.com>
2012-10-25 15:22:28 +0200

    complete spec for configuration

--- spec/s3db/configuration_spec.rb
+++ spec/s3db/configuration_spec.rb
@@ -7,9 +7,47 @@ describe S3db::Configuration do
     stub_s3_config_yml()
   end
 
-  it "load the db config" do
-    config = S3db::Configuration.new
-    config.db.should == rails_configurations['test']
+  describe "initialize" do
+    it "loads the db config" do
+      config = S3db::Configuration.new
+      config.db.should == rails_configurations['test']
+    end
+
+    it "loads the aws config" do
+      config = S3db::Configuration.new
+      config.aws.should == s3_config_yml_contents.merge({'bucket' => 's3db_backup_test_bucket'})
+    end
+
+    describe "s3 config yml" do
+      describe "first-level key is missing" do
+        it "throws AwsConfigurationError" do
+          should_raise_error_if_missing_key('test')
+          should_raise_error_if_missing_key('aws_access_key_id')
+          should_raise_error_if_missing_key('secret_access_key')
+        end
+      end
+
+      describe "bucket is missing" do
+        it "throws AwsConfigurationError" do
+          incomplete_s3_config = s3_config_yml_contents
+          env_config = incomplete_s3_config['test']
+          env_config.delete('bucket')
+          incomplete_s3_config['test'] = env_config
+          YAML.stub(:load_file => incomplete_s3_config)
+
+          expect { S3db::Configuration.new }.to raise_error(S3db::AwsConfigurationError, /bucket/)
+        end
+      end
+    end
+
   end
+end
+
+def should_raise_error_if_missing_key(key)
+  incomplete_s3_config = s3_config_yml_contents
+  incomplete_s3_config.delete(key)
+  YAML.stub(:load_file => incomplete_s3_config)
+
+  expect { S3db::Configuration.new }.to raise_error(S3db::AwsConfigurationError, /#{key}/)
 end
 
--- spec/support/s3_config.rb
+++ spec/support/s3_config.rb
@@ -1,7 +1,11 @@
-def stub_s3_config_yml
-  YAML.stub(:load_file => {
+def s3_config_yml_contents
+  {
       "test" => {"bucket" => "s3db_backup_test_bucket"},
       "secret_access_key" => "secret_access_key",
       "aws_access_key_id" => "access_key_id"
-  })
+  }
+end
+
+def stub_s3_config_yml
+  YAML.stub(:load_file => s3_config_yml_contents())
 end

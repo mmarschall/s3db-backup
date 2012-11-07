a19c0819c774308671c678e481b608edb25cf28c
Matthias Marschall <mm@agileweboperations.com>
2012-11-07 16:59:18 +0100

    adding more specs for S3dbBackup

--- spec/s3db-backup_spec.rb
+++ spec/s3db-backup_spec.rb
@@ -2,16 +2,40 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "S3dbBackup" do
 
-  let(:backup) { double("backup") }
 
-  before do
-    S3dbBackup.stub(:backup_instance => backup)
-  end
 
   describe ".backup" do
+
+    let(:backup) { double("backup") }
+
+    before do
+      S3dbBackup.stub(:backup_instance => backup)
+    end
+
     it "calls the backup method on the backup instance" do
       backup.should_receive(:backup)
       S3dbBackup.backup
     end
   end
+
+  describe ".fetch" do
+
+    let(:fetcher) { double("fetcher") }
+
+    it "calls the fetch method on the fetcher instance" do
+      fetcher.should_receive(:fetch)
+      S3dbBackup.fetch
+    end
+  end
+
+  describe ".load" do
+
+    let(:loader) { double("loader") }
+
+    it "calls the load method on the loader instance" do
+      loader.should_receive(:load)
+      S3dbBackup.load
+    end
+  end
+
 end
